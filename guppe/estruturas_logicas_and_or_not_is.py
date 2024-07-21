dc3a699b34f7928f203dd3fc5c7ae88d612adc51
Jobson Almeida <jobsonbox@gmail.com>
2024-07-21 20:29:45 -0300

    logic operators (and, or, not and is) completed

---
+++ guppe/estruturas_logicas_and_or_not_is.py
@@ -0,0 +1,28 @@
+"""
+Estruturas lógias: and (e), or(ou), not (não), is (é)
+
+Operadores unários:
+    - not
+Operadores binários:
+    - and, or, is
+
+"""
+
+ativo = False
+logado = True
+
+if not ativo and not logado:
+    print("Bem vindo usuário")
+else:
+    print("Você precisa ativar sua conta. Por favor, cheque seu e-mail")
+
+print(not True)
+
+print("Checking is operator:")
+if ativo is False:
+    print("Você precisa ativar sua conta. Por favor, cheque seu e-mail")
+
+
+
+
+
