4cca120ad00b23acf22fbde747577926dd0dd27c
Jobson Almeida <jobsonbox@gmail.com>
2024-07-21 18:34:33 -0300

    type boolean completed

---
+++ guppe/tipo_booleano.py
@@ -0,0 +1,40 @@
+"""
+Tipo Booleano
+
+Álgebra booleana criada por George Boole
+
+True -> verdadeiro
+False -> falso
+
+obs.: sempre com a inicial maiúscula
+"""
+
+ativo = False
+
+print(ativo)
+
+"""
+Operações básicas
+"""
+
+# Negação (not)
+
+estado = not ativo
+
+print(estado)
+
+# Operação or
+# It's a binary operation
+
+logado = False
+
+print("ativo or logado")
+print(ativo or logado)
+
+# Operação and
+
+print (ativo and logado)
+
+# Comparação
+
+
