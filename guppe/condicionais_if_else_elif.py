6b25dff9af12d526c8a28c1e866bad1c05d7fa9a
Jobson Almeida <jobsonbox@gmail.com>
2024-07-21 20:05:44 -0300

    if, else, elif completed

---
+++ guppe/condicionais_if_else_elif.py
@@ -0,0 +1,28 @@
+"""
+Estruturas condicionais
+if, else, elif
+"""
+
+"""
+#Estrutura condicional em C
+if(idade <18){
+    printf("Menor de idade")
+}
+"""
+
+idade = 16
+
+if idade < 18:
+    print("Menor de idade")
+    print("dada")
+elif idade == 18:
+    print("Tem 18 anos")
+elif idade == 26:
+    print("Tem 26 anos")
+else:
+    print("Maior de idade")
+
+if idade < 18:
+    print("Menor de idade")
+
+
