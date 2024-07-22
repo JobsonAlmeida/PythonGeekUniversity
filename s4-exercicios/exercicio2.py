55544e91e62325e31ecd7657566b9df7d71da854
Jobson Almeida <jobsonbox@gmail.com>
2024-07-22 08:07:20 -0300

    Exercise2 was refactored

--- s4-exercicios/exercicio2.py
+++ s4-exercicios/exercicio2.py
@@ -9,7 +9,7 @@ import math
 num = int(input("The the number you want: "))
 
 if num >= 0:
-    print(f"The square root of {num} is", math.sqrt(num))
+    print(f'The square root of {num} is', round(math.sqrt(num), 2))
 else:
     print(f"The number {num} is invalid.")
 
