1a2575753f45b318b2c1fdb05352b41b67d182db
Jobson Almeida <jobsonbox@gmail.com>
2024-07-25 15:37:48 -0300

    Lists completed

--- guppe/listas.py
+++ guppe/listas.py
@@ -154,6 +154,208 @@ curso = "$".join(lista6)
 print(curso)
 
 
+#Podemos misturar tipos de dados colocados na lista
+lista6 = [1, 2.34, True, "geek", 'd', [1, 2, 3], 828289238492492]
+
+#Iterando sobre listas
+
+lista1 = [1, 99, 4, 27, 15, 34, 42, 45, 55, 43 ]
+print("--- iterando sobre listas ---")
+for elemento in lista1:
+    print(elemento)
+
+
+# carrinho = []
+# produto = ''
+#
+# while produto != 'sair':
+#     print("Adicione um produto na lista ou digite 'sair' para sair: ")
+#     produto = input()
+#     if produto != 'sair':
+#         carrinho.append(produto)
+
+# Utilizand varivaveis em listas
+
+numeros = [2, 3, 4, 5]
+
+num1 = 1
+num2 = 2
+num3 = 3
+num4 = 4
+num5 = 5
+
+numeros = [num1, num2, num3, num4, num5]
+
+print(numeros)
+
+#Fazemos acesso aos elementos de forma idexada
+
+cores = [ "verde", 'amarelo', 'azul', 'branco']
+
+print(cores[0])
+print(cores[1])
+print(cores[2])
+print(cores[3])
+
+# Fazemos acesso aos elementos de forma indexada inversa
+# Para entender o índice negativo pense na lista como um círculo onde o final está ligado ao ínicio
+print(cores[-4])
+print(cores[-3])
+print(cores[-2])
+print(cores[-1])
+
+# # Gera erro neste caso
+# print(cores[-8])
+# print(cores[-7])
+# print(cores[-6])
+# print(cores[-5])
+
+
+print("-----------------")
+cores = ["verde", 'amarelo', 'azul', 'branco']
+
+for cor in cores:
+    print(cor)
+
+indice = 0
+while indice < len(cores):
+    print(cores[indice])
+    indice = indice + 1
+
+#Gerar indice em um for
+cores = ["verde", 'amarelo', 'azul', 'branco']
+
+for indice, cor in enumerate(cores):
+    print(indice, cor)
+
+# Listas aceitam valores repetidos
+lista = []
+lista.append(42)
+lista.append(42)
+lista.append(42)
+lista.append(42)
+lista.append(33)
+lista.append(33)
+
+print(lista)
+
+print("---")
+# Métodos não tão importantes, mas também úteis
+
+#Encontrar o índice de um elemento na lista
+
+numeros = [5, 6, 5, 8, 8, 3, 2, 0, 5]
+
+print(numeros.index(6))
+print(numeros.index(5))
+# print(numeros.index(600)) # Se o elemento não estiver na lista é retornado um erro
+
+# Podemos fazer busca dentro de um range, ou seja qual indice começar a buscar
+
+print(" ----busca ----")
+print(numeros.index(5, 3))
+
+#Podemos fazer busca dentro de um range inicio e fim
+print(numeros.index(6, 1, 10))
+
+#Slicing
+#lista[inicio:fim:passo]
+
+numeros = [5, 6, 5, 8, 8, 3, 2, 0, 5]
+print(numeros[0:10:2])
+print(numeros)
+
+
+#Trabalhando com o parâmetro início
+print("---trabalhando com o parêmtro inicio----")
+lista = [1,2,3,4,5,6]
+print(lista[1:])
+print(lista[-1:])
+print(lista[-3:])
+
+#Trabalhando com o parâmetro fim
+print("---trabalhando com o parêmtro fim----")
+lista = [1,2,3,4,5,6]
+print(lista[:2]) #começa no início da lista (índice 0)  e vai até o índice 2-1
+print(lista[:-1])
+print(lista[:-3:2]) # podemos incluir um passo
+
+#Trabalhando com o passo
+
+print(lista[1::2]) #Começa no índice 1 e vai até o final da lista de 2 em 2
+print(lista[::2]) #Começa no início da lista (índice 0) e vai até o final de 2 em 2
+print(lista[2::-1]) #Começa do índíce 1 e vai até o final da lista com passo de -1.
+print(lista[0::1])
+
+
+#Invertendo valores em uma lista
+print("---invertendo valores---")
+nomes = ['Geek', 'University']
+nomes[0], nomes[1] = nomes[1], nomes[0] #Interesting way to change values whithout an auxiliar variable
+print(nomes)
+
+#Soma*, valor máximo*, valor mínimo*, tamanho
+# Se os valores forem todos inteiros ou reais
+
+lista = [1,2,3,4,5,6]
+print(sum(lista))
+print(max(lista))
+print(min(lista))
+print(len(lista))
+
+# Transfomando uma lista em tupla
+
+lista = [1,2,3,4,5,6]
+print(lista)
+print(type(lista))
+
+tupla = tuple(lista)
+print(tupla)
+print(type(tupla))
+
+# Desempacotamento de listas
+lista = [1,2,3]
+num1, num2, num3 = lista
+
+print(num1)
+print(num2)
+print(num3)
+
+# Obs.: Se tivermos um número de elementos na lista diferente em comparação com o número de variáveis para receber os dados, recebemos um value error.
+
+# Copiando uma lista para outra (shallow copy e deep copy)
+print("---Copiando listas---")
+lista = [1,2,3]
+print(lista)
+
+nova = lista.copy()
+print(nova)
+
+nova.append(4)
+print(lista)
+print(nova)
+
+print("---fazendo a copia----")
+lista1 = [1, 2, ['a', 'b'], 3, 4, 5, 6]
+lista2 = lista1.copy()
+
+print(lista1)
+print(lista2)
+
+print("--- depois  de inserir um novo elemento---")
+
+lista1[2].append("xyz")
+print(lista1)
+print(lista2)
+
+
+
+
+
+
+
+
+
 
 
 
