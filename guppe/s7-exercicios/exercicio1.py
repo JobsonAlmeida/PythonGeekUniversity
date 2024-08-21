b5520fa18e8f4f69e9cc0090b85c23c2c62b310f
Jobson Almeida <jobsonbox@gmail.com>
2024-08-21 19:41:08 -0300

    'exercicio1' completed

--- guppe/s7-exercicios/exercicio1.py
+++ guppe/s7-exercicios/exercicio1.py
@@ -1,3 +1,12 @@
 """
 1. Crie um programa que tenha uma função que recebe um parâmetro inteiro e devolve o seu dobro.
 """
+
+def func_double(number):
+    return number*2
+
+#testando
+print(func_double(2))
+print(func_double(3))
+print(func_double(4))
+print(func_double(5))
