285348aae1c7128710984a4b851b707b2121d13d
Jobson Almeida <jobsonbox@gmail.com>
2024-08-21 19:57:56 -0300

    'exercicio3' completed

--- guppe/s7-exercicios/exercicio3.py
+++ guppe/s7-exercicios/exercicio3.py
@@ -1,3 +1,11 @@
 """
 3. Faça um programa que tenha uma função que receba uma lista de inteiros e retorne o maior valor
 """
+
+lista1 = [10, 20, 9, 8, 1, 2, 19, 46, 6]
+
+def retorna_maior(lista):
+    return max(lista)
+
+#executando a função
+print(retorna_maior(lista1))
