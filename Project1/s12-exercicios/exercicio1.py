5f916e63f22a3d4214f5b18fcc4dfd8affecdb48
Jobson Almeida <jobsonbox@gmail.com>
2024-08-29 13:36:12 -0300

    exercice 1 has been completed

--- Project1/s12-exercicios/exercicio1.py
+++ Project1/s12-exercicios/exercicio1.py
@@ -6,3 +6,29 @@ b) Permita que o usuário grave diversos caracteres nesse arquivo, até que o us
 c) Feche o arquivo
 d) Abra, leia o arquivo e escreva na tela todos os caracteres armazenados.
 """
+
+#a)
+
+arquivo = open("arq.txt", 'a')
+
+# b)
+
+while True:
+    texto = input("Digite o texto: ")
+
+    if texto == '0':
+        break;
+    
+    arquivo.write(texto)
+    arquivo.write("\n")
+
+# c)
+arquivo.close()
+
+# d)
+
+arquivo  = open("arq.txt", 'r')
+texto = arquivo.read()
+print(texto)
+
+arquivo.close()
