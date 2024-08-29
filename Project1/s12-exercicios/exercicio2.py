72ae6449f3acf58fdf673da00ad75266f58b6ebe
Jobson Almeida <jobsonbox@gmail.com>
2024-08-29 13:13:22 -0300

    Exercises files about section 12 has been created

---
+++ Project1/s12-exercicios/exercicio2.py
@@ -0,0 +1,4 @@
+"""
+2. Faça um programa que receba do usuário o nome de um arquivo texto e mostre na tela quantas letras são 
+vogais e quantas são consoantes.
+"""
---
+++ Project1/s12-exercicios/exercicio3.py
@@ -0,0 +1,5 @@
+"""
+3. Faça um programa que receba do usuário o nome de um arquivo texto e mostre na tela quantas linhas este 
+arquivo possui.
+
+"""
