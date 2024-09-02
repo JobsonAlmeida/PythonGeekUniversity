8e89526d035e5d9b8f711baee36b721fde2aff96
Jobson Almeida <jobsonbox@gmail.com>
2024-09-02 12:04:15 -0300

    Lesson on classes has been completed

---
+++ Project1/classes.py
@@ -0,0 +1,24 @@
+"""
+POO
+
+
+
+Quando estamos planejando um software e definimos quais classes teremos que ter chamados os objetos que teremos que mapear para as classes de entidade
+
+
+"""
+
+class Lampada:
+    pass
+
+class ContaCorrente:
+    pass
+
+print(type(Lampada))
+print(type(ContaCorrente))
+
+valor  = int(42)
+
+print(help(int))
+
+
