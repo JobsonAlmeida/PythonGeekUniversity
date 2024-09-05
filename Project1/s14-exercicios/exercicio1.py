5569b5ce24bcc097e302016387e257f7d6a3210a
Jobson Almeida <jobsonbox@gmail.com>
2024-09-05 11:10:21 -0300

    Exercise 1 from chapter 14 has been done

--- Project1/s14-exercicios/exercicio1.py
+++ Project1/s14-exercicios/exercicio1.py
@@ -4,3 +4,35 @@ setters para os atributos e um método para imprimir os dados de uma pessoa.
 
 """
 
+class Pessoa:
+
+    def __init__(self, nome, data_nascimento, email):
+        self.__nome = nome
+        self.__data_nascimento = data_nascimento
+        self.__email = email
+
+    def get_nome(self):
+        return self.__nome
+
+    def set_nome(self, nome):
+        self.__nome = nome
+
+    def get_data_nascimento(self):
+        return self.__data_nascimento
+
+    def set_data_nascimento(self, data_nascimento):
+        self.__data_nascimento = data_nascimento
+
+    def get_email(self, email):
+        return self.__email
+
+    def set_email(self, email):
+        self.__email = email
+
+    def print(self):
+        print(f"Nome: {self.__nome}\nData de nascimento: {self.__data_nascimento}\nEmail: {self.__email}")
+
+
+pessoa1 = Pessoa("Jose da Silva", "29/11/1988", "jose@gmail.com")
+
+pessoa1.print()
