cb456593a4f09243f213d7c0ca5264be7f0c738a
Jobson Almeida <jobsonbox@gmail.com>
2024-09-08 15:17:03 -0300

    Lesson 3 has been completed

--- Project1/s16-exercicios/exercicio3.py
+++ Project1/s16-exercicios/exercicio3.py
@@ -1,3 +1,20 @@
 """
 3. Crie um programa, instancie um objeto da classe Carro e teste o seu médodo
 """
+
+from exercicio2 import Carro
+
+if __name__ == "__main__":
+    carro1 = Carro("Honda", "HR-V", "GHD56845")
+
+    print(carro1)
+
+    print(carro1.marca)
+    carro1.marca = "BMW"
+    print(carro1.marca)
+
+    print(carro1.modelo)
+    carro1.modelo = "Astra"
+    print(carro1.modelo)
+
+    print(carro1)
